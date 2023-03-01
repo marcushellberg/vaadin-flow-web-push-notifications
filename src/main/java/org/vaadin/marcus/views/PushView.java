d50542586defd90fa17c506d75e32da8e173bf65
Marcus Hellberg <marcus@vaadin.com>
2023-03-01 10:30:22 -0800

    Simplify code

--- src/main/java/org/vaadin/marcus/views/PushView.java
+++ src/main/java/org/vaadin/marcus/views/PushView.java
@@ -7,7 +7,6 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import nl.martijndwars.webpush.Subscription;
 import org.vaadin.marcus.webpush.WebPushService;
 import org.vaadin.marcus.webpush.WebPushToggle;
 
@@ -15,37 +14,27 @@ import org.vaadin.marcus.webpush.WebPushToggle;
 @Route(value = "")
 public class PushView extends VerticalLayout {
 
-    private final WebPushService webPushService;
-
     public PushView(WebPushService webPushService) {
-        this.webPushService = webPushService;
 
         var toggle = new WebPushToggle(webPushService.getPublicKey());
         var messageInput = new TextField("Message:");
         var sendButton = new Button("Notify all users!");
+        var messageLayout = new HorizontalLayout(messageInput, sendButton);
+        messageLayout.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
 
         add(
-                new H1("Web Push Notification Demo"),
-                toggle,
-                new HorizontalLayout(messageInput, sendButton) {{setDefaultVerticalComponentAlignment(Alignment.BASELINE);}}
+            new H1("Web Push Notification Demo"),
+            toggle,
+            messageLayout
         );
 
         toggle.addSubscribeListener(e -> {
-            subscribe(e.getSubscription());
+            webPushService.subscribe(e.getSubscription());
         });
         toggle.addUnsubscribeListener(e -> {
-            unsubscribe(e.getSubscription());
+            webPushService.unsubscribe(e.getSubscription());
         });
 
         sendButton.addClickListener(e -> webPushService.notifyAll("Message from user", messageInput.getValue()));
     }
-
-    public void subscribe(Subscription subscription) {
-        webPushService.subscribe(subscription);
-    }
-
-    public void unsubscribe(Subscription subscription) {
-        webPushService.unsubscribe(subscription);
-    }
-
 }
